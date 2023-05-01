ba32dccff9e4994ed29af22bd8185310a8d6a806
FrederikvSvane <s224766@student.dtu.dk>
2023-05-01 16:13:14 +0200

    First scenario in project feature working
    All the shit

--- src/main/java/application/ProjectMenu.java
+++ src/main/java/application/ProjectMenu.java
@@ -1,19 +1,26 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
+import application.DateServer;
+
 public class ProjectMenu{
 
-
+    private static DateServer dateServer;
 
     private List<Project> projects = new ArrayList<Project>();
 
+    private static HashMap<Integer, Integer> projectsCreatedInYear = new HashMap<Integer, Integer>();
 
-    public ProjectMenu() {
+
+    public void setDateServer(DateServer dateServer) { this.dateServer = dateServer;}
+
+    public static Calendar getDate() {
+        return dateServer.getDate();
     }
-
 
 
 //    public void displayMenu(){
@@ -21,8 +28,9 @@ public class ProjectMenu{
 //    }
 
 
-    public void addProject(Project projectName){
-        projects.add(projectName);
+    public void addProject(String projectName){
+        Project tempProject = new Project(projectName);
+        projects.add(tempProject);
     }
 
 
@@ -30,4 +38,15 @@ public class ProjectMenu{
         return projects.stream().anyMatch(p -> p.getProjectName().equals(projectName));
     }
 
+    public static int getProjectsCreatedInYear(int year) {
+    	if(projectsCreatedInYear.get(year)==null) {
+    		return 0;
+    	}else {
+    		return projectsCreatedInYear.get(year);
+    	}
+    }
+
+    public Project getProject(String projectName) {
+        return projects.stream().filter(p -> p.getProjectName().equals(projectName)).findFirst().get();
+    }
 }
