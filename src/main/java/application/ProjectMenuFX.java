bc145665ba43ed3486b5cb9fb7cf3370ae8d624b
ssschoubye <116802763+ssschoubye@users.noreply.github.com>
2023-05-01 16:42:14 +0200

    Hurtig ting

--- src/main/java/application/ProjectMenuFX.java
+++ src/main/java/application/ProjectMenuFX.java
@@ -45,9 +45,9 @@ public class ProjectMenuFX extends Application {
     @FXML
     Button newProject2;
     @FXML
-    Button addActivity1;
+    Button newActivity1;
     @FXML
-    Button addActivity2;
+    Button newActivity2;
     @FXML
     Button deleteActivity1;
     @FXML
@@ -56,8 +56,17 @@ public class ProjectMenuFX extends Application {
     Button addHours1;
     @FXML
     Button addHours2;
+    @FXML
+    Button logOut1;
+    @FXML
+    Button logOut2;
 
     public void addProject(){
+        ObservableList<String> items = FXCollections.observableArrayList("Item 1", "Item 2", "Item 3");
+        projectList.setItems(items);
+
+    }
+    public void addProject2(){
         ObservableList<String> items = FXCollections.observableArrayList("Item 1", "Item 2", "Item 3");
         projectList.setItems(items);
 
@@ -67,8 +76,16 @@ public class ProjectMenuFX extends Application {
         projectList.setItems(items);
         System.out.println(projectList.getOnMouseClicked());
     }
-
+    public void updateList2(){
+        ObservableList<String> items = FXCollections.observableArrayList("Item 1", "Item 2", "Item 3");
+        projectList.setItems(items);
+        System.out.println(projectList.getOnMouseClicked());
+    }
     public void projectSelect(){
+        String selectedItem = projectList.getSelectionModel().getSelectedItem();
+        System.out.println("Clicked on: " + selectedItem);
+    }
+    public void projectSelect2(){
         String selectedItem = projectList.getSelectionModel().getSelectedItem();
         System.out.println("Clicked on: " + selectedItem);
     }
