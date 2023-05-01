ee0af539bbd9505f8065939fc6921ed46f140f68
ssschoubye <116802763+ssschoubye@users.noreply.github.com>
2023-05-01 15:13:06 +0200

    All kinds of stuff

--- src/main/java/application/ProjectMenuFX.java
+++ src/main/java/application/ProjectMenuFX.java
@@ -1,14 +1,24 @@
 package application;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+
+import javax.swing.text.View;
 
 import java.io.IOException;
 
 public class ProjectMenuFX extends Application {
+
+    String[] list = new String[10];
+
     public void newStart() throws IOException {
         Stage stage = new Stage();
         start(stage);
@@ -18,8 +28,30 @@ public class ProjectMenuFX extends Application {
     public void start(Stage stage) throws IOException {;
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("ProjectMenu.fxml"));
         Scene scene = new Scene(root);
-
         stage.setScene(scene);
         stage.show();
+
+    }
+
+
+    @FXML
+    ListView<String> projectList;
+    @FXML
+    Button update;
+
+    public void addProject(){
+        ObservableList<String> items = FXCollections.observableArrayList("Item 1", "Item 2", "Item 3");
+        projectList.setItems(items);
+
+    }
+    public void updateList(){
+        ObservableList<String> items = FXCollections.observableArrayList("Item 1", "Item 2", "Item 3");
+        projectList.setItems(items);
+        System.out.println(projectList.getOnMouseClicked());
+    }
+
+    public void projectSelect(){
+        String selectedItem = projectList.getSelectionModel().getSelectedItem();
+        System.out.println("Clicked on: " + selectedItem);
     }
 }
