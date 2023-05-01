14e50de8198af52f131bc57d5b3233ed855128d9
ssschoubye <116802763+ssschoubye@users.noreply.github.com>
2023-05-01 11:09:45 +0200

    Commit1

--- src/main/java/application/Driver.java
+++ src/main/java/application/Driver.java
@@ -1,45 +1,38 @@
 package application;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Driver extends Application {
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+public class Driver extends Application{
 
     private AuthenticationService authenticationService;
     private EmployeeBase employeeBase;
-
+/*
     public Driver(String username) {
         authenticationService = new AuthenticationService(username);
         employeeBase = new EmployeeBase();
     }
+*/
 
 
+
+    public static void main(String[] args) throws IOException {
+      launch(args);
+    }
     @Override
-    public void start(Stage stage) {
-        Label l = new Label("Indtast dit brugernavn her");
-        TextField t = new TextField();
-        GridPane grid = new GridPane();
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        grid.setAlignment(Pos.CENTER);
-        grid.add(l,0,0);
-        grid.add(t,0,1);
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        Scene scene = new Scene(root);
 
-        //Label l = new Label("Lav en ny bruger");
-        Scene scene = new Scene(grid, 640, 480);
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 
 }
--- src/main/java/application/DriverFX.java
+++ src/main/java/application/DriverFX.java
@@ -8,17 +8,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class DriverFX extends Application {
+public class DriverFX {
 
-    public void newStart(){
-        Stage stage = new Stage;
-        start(stage);
-    }
-    public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
-        stage.show();
-    }
 }
