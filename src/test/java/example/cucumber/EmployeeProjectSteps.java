8763c38c0e97293241dcf0a3c94dc87226f76151
ssschoubye <116802763+ssschoubye@users.noreply.github.com>
2023-05-02 14:00:04 +0200

    Update EmployeeProjectSteps.java

--- src/test/java/example/cucumber/EmployeeProjectSteps.java
+++ src/test/java/example/cucumber/EmployeeProjectSteps.java
@@ -1,4 +1,40 @@
 package example.cucumber;
+import application.AuthenticationService;
+import application.ProjectMenu;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class EmployeeProjectSteps {
+    @Given("A user is logged in and has a project selected.")
+    public void a_user_is_logged_in_and_has_a_project_selected() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("A user is created with the initials “lusj“.")
+    public void a_user_is_created_with_the_initials_lusj() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("An existing user adds a new employee to a project")
+    public void an_existing_user_adds_a_new_employee_to_a_project() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("The system throws an error, because the employee is already assigned to the project")
+    public void the_system_throws_an_error_because_the_employee_is_already_assigned_to_the_project() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
